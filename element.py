3098444896e468f44548c50f19df7d69e0efb7b4
abarnaramaraj <47477481+abarnaramaraj@users.noreply.github.com>
2019-05-05 10:37:00 +0530

    Create element.py

---
+++ element.py
@@ -0,0 +1,11 @@
+a[]
+n=int(input("enter the num"))
+for i in range(1,n+1):
+ b=int(input("enter the element"))
+ a.append(b)
+ k=0
+num=int(input("enter the num"))
+for j in a:
+ if(j==num):
+ k=k+1
+print("num of times")
